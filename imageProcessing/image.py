7b990f29a99c05ede2e5ff6a926e3fd752f81d07
lenovo <wangwei@gmx.com>
2019-11-04 23:13:22 +0800

    gfg

---
+++ imageProcessing/image.py
@@ -0,0 +1,12 @@
+from PIL import Image
+from pylab import *
+pil_im=array(Image.open('timg.jfif').convert('L'))
+
+figure()
+gray()
+contour(pil_im,origin='image')
+axis('equal')
+axis('off')
+figure()
+hist(pil_im.flatten(),128)
+show()
