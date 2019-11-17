90d43f4c9e23c7bca048a2d8403c256d3d8f77dc
lenovo <wangwei@gmx.com>
2019-11-17 15:20:23 +0800

    gfg

--- imageProcessing/image.py
+++ imageProcessing/image.py
@@ -1,12 +1,13 @@
 from PIL import Image
 from pylab import *
-pil_im=array(Image.open('timg.jfif').convert('L'))
+
+pil_im = array(Image.open('timg.jfif').convert('L'))
 
 figure()
 gray()
-contour(pil_im,origin='image')
+contour(pil_im, origin='image')
 axis('equal')
 axis('off')
 figure()
-hist(pil_im.flatten(),128)
+hist(pil_im.flatten(), 128)
 show()
